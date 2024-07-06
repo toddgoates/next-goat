164b3bcc53a987ab34fd025e2faac91c108220df
Todd Goates <toddgoates@gmail.com>
2024-07-06 14:53:47 -0600

    Fix alert tailwind styling issue.

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -3,23 +3,26 @@ type AlertProps = {
   children: React.ReactNode;
 };
 
-export default function Alert({ type, children }: AlertProps) {
-  const colorMap = {
-    success: "purple",
-    info: "indigo",
-    warning: "orange",
-    error: "red",
-  };
+export default function Alert({ type = "info", children }: AlertProps) {
+  let theme = "";
 
-  const bgColor = `bg-${colorMap[type]}-200`;
-  const color = `text-${colorMap[type]}-800`;
-  const borderColor = `border-${colorMap[type]}-800`;
+  switch (type) {
+    case "success":
+      theme = "bg-purple-200 text-purple-800 border-purple-800";
+      break;
+    case "info":
+      theme = "bg-indigo-200 text-indigo-800 border-indigo-800";
+      break;
+    case "warning":
+      theme = "bg-orange-200 text-orange-800 border-orange-800";
+      break;
+    case "error":
+      theme = "bg-red-200 text-red-800 border-red-800";
+      break;
+  }
 
   return (
-    <div
-      role="alert"
-      className={`p-5 border-l-4 ${bgColor} ${color} ${borderColor}`}
-    >
+    <div role="alert" className={`p-5 border-l-4 ${theme}`}>
       {children}
     </div>
   );
