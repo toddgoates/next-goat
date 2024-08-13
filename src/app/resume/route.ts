5936d978489ab355681ef236711b873f02f7ee2e
Todd Goates <toddgoates@gmail.com>
2024-08-13 15:47:51 -0600

    Add a route for downloading resume.

---
+++ src/app/resume/route.ts
@@ -0,0 +1,14 @@
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+    const filePath = path.join(process.cwd(), 'public', 'files', 'TMG-2024.pdf');
+    const fileBuffer = fs.readFileSync(filePath);
+
+    return new Response(fileBuffer, {
+        headers: {
+            'content-type': 'application/pdf',
+            'content-disposition': 'inline; filename="Todd-Goates-Resume.pdf"',
+        },  
+    });
+}
